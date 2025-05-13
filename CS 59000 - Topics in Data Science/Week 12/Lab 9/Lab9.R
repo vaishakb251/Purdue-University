dba9fb15f908c17cb04cf1d5640cea92bd8ee325
Vaishak Balachandra <157973509+vaishakb251@users.noreply.github.com>
2025-05-13 05:05:48 -0500

    Add files via upload

---
+++ CS 59000 - Topics in Data Science/Week 12/Lab 9/Lab9.R
@@ -0,0 +1,78 @@
+# Lab 9
+
+# 1
+
+# a
+install.packages("palmerpenguins")
+library(palmerpenguins)
+data(penguins, package = "palmerpenguins")
+head(penguins)
+dim(penguins)
+
+# b
+names(penguins)
+attach(penguins)
+table(species)
+cat("There are 3 species of penguins in the given dataset")
+
+# c
+new = penguins[,c(1,3,5)]
+head(new)
+dim(new)
+names(new)
+cleandata <- na.omit(new)
+head(cleandata)
+dim(cleandata)
+
+# also
+needed_data <- cleandata[,c(2,3)]
+head(needed_data)
+dim(needed_data)
+class = cleandata$species
+head(class)
+length(class)
+class
+
+# d
+data1 <- scale(needed_data)
+head(data1)
+
+
+
+# e
+install.packages("factoextra")
+library(factoextra)
+fviz_nbclust(data1, kmeans, method="wss")
+cat("From the plot, 3 or 4 is the best cluster count")
+
+# f
+result = kmeans(data1,3)
+result$size
+result
+cat("For 3 clusters: 127 151  64")
+
+result1 = kmeans(data1,4)
+result1$size
+result1
+cat("For 4 clusters: 41 116 116  69")
+
+# g
+# For 3 clusters
+result$centers
+result$cluster
+result$size
+
+# # For 4 clusters
+# result1$centers
+# result1$cluster
+# result1$size
+
+# h
+result$cluster
+length(result$cluster)
+table(result$cluster, class)
+table(class)
+cat("Thus, we can see that: FOR 3 CLUSTER SYSTEM
+1. Out of 151 actual Adelie species, 146 were correctly identified and rest 5 were wrongly identified.
+2. Out of 68 actual Chinstrap species, 59 were correctly identified and rest 9 were wrongly identified.
+3. Out of 123 actual Gentoo species, 122 were correctly identified and rest 1 is wrongly identified.")
